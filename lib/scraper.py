9c6b27fe2810b4bb82ce557feb5fc9a997168e53
Victor <victor.kihiko@student.moringaschool.com>
2025-03-27 17:11:13 +0300

    scraping data in html

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,8 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+doc.select('.heading-60-black.color-black.mb-20')[0].attrs
+print(doc)
+
